ca6eab3f52bab00f03f10be4f758bd7552f907d9
rashmikanaveen <rashmikanaveen734@gmail.com>
2024-07-13 19:39:05 +0530

    2

--- part1/courseinfo/part1/src/App.jsx
+++ part1/courseinfo/part1/src/App.jsx
@@ -1,35 +1,57 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
+const Header = (props) => {
+  // const-definitions
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <h1>{props.course}</h1>
+    </div>
   )
 }
 
+
+const Content = (props) => {
+  // const-definitions
+
+  return (
+    <div>
+      <br />
+      <hr />
+      <p>This is content</p>
+      <hr />
+      <br />
+    </div>
+  )
+}
+
+const Total = (props) => {
+  // const-definitions
+
+  return (
+    <div>
+      <br />
+      <hr />
+      <p>This is Total</p>
+      <hr />
+      <br />
+    </div>
+  )
+}
+
+
+
+
+const App = () => {
+  // const-definitions
+  const course = 'Half Stack application development'
+
+  console.log('Hi')
+  return (
+    <div>
+      <Header course={course} />
+      <Content  />
+      <Total />
+      
+    </div>
+  )
+}
 export default App
