a16e99a84c2f37696eb254f4761401fc83314c21
rashmikanaveen <rashmikanaveen734@gmail.com>
2024-07-14 14:08:19 +0530

    create link and improvements

--- part1/courseinfo/part1/src/App.jsx
+++ part1/courseinfo/part1/src/App.jsx
@@ -6,14 +6,11 @@ const Header = (props) => {
 
 const Content = (props) => {
   // const-definitions
+  console.log(props)
 
   return (
     <div>
-      <br />
-      <hr />
-      <p>This is content</p>
-      <hr />
-      <br />
+      <p><a href={props.link}>{props.name}</a> has {props.exercises} exercises to do</p>
     </div>
   )
 }
@@ -36,17 +33,37 @@ const Total = (props) => {
 
 
 const App = () => {
-  // const-definitions
   const course = 'Half Stack application development'
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
 
-  console.log('Hi')
   return (
     <div>
-      <Header course={course} />
-      <Content  />
-      <Total />
+      <Header course= {course}/>
+      <br />
+      <ol>
+        <li>      <Content name={part1.name} exercises={part1.exercises} link ='https://fullstackopen.com/en/part1'/></li>
+        <li>      <Content name={part2.name} exercises={part2.exercises}link ='https://fullstackopen.com/en/part2' /></li>
+        <li>      <Content name={part3.name} exercises={part3.exercises} link='https://fullstackopen.com/en/part3' /></li>
+        
+        
+      </ol>
+
+
       
     </div>
   )
 }
+
+
 export default App
