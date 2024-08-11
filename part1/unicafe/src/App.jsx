0efcda84204bc53d15327e7f001de39ca13fd843
rashmikanaveen <rashmikanaveen734@gmail.com>
2024-08-11 19:48:51 +0530

    done 1.6: unicafe step 1

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,30 +1,42 @@
 import { useState } from 'react'
 
-const Button = (props) => (
-  <button onClick={props.handleClick}>
-    {props.text}
-  </button>
-)
-
-const App = () => {
-  const [value, setValue] = useState(10)
-
-  const setToValue = newValue => {
-    console.log('value now', newValue)
-    setValue(newValue)
+const Display =({ good, neutral, bad })=>{
+  if(good===0 && neutral===0 && bad===0){
+    return(
+      <div>
+        <p>No feedback given</p>
+      </div>
+    )
   }
-
-  // Do not define components inside another component
-  const Display = props => <div>{props.value}</div>
+  return(
+    <div>
+      
+      <p>good {good} <br />
+      neutral {neutral} <br />
+      bad {bad}</p>
+    </div>
+  )
+}
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
 
   return (
     <div>
-      <Display value={value} />
-      <Button handleClick={() => setToValue(1000)} text="thousand" />
-      <Button handleClick={() => setToValue(0)} text="reset" />
-      <Button handleClick={() => setToValue(value + 1)} text="increment" />
+      <h1>give feed back</h1>
+      <button onClick={()=>setGood(good+1)}>good</button>
+      <button onClick={()=>setNeutral(neutral+1)}>neutral</button>
+      <button onClick={()=>setBad(bad+1)}>bad</button>
+      <h2>Statistics</h2>
+      <Display good={good} neutral={neutral} bad={bad}/>
+      
+      
+      
     </div>
   )
 }
 
 export default App
+
