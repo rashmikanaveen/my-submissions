e55f00d64b967d9c9ea78ff8ebbeb045067512d4
rashmikanaveen <rashmikanaveen734@gmail.com>
2024-09-16 20:23:42 +0530

    create new countries project

---
+++ part2/countries/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+
+
+
+
+function App() {
+  const [count, setCount] = useState(0)
+  console.log('hello')
+
+  return (
+    <>
+      hello
+    </>
+  )
+}
+
+export default App
