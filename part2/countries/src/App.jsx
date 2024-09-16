2851b6d4c6e1732abff3f3479ee40e0e52548263
rashmikanaveen <rashmikanaveen734@gmail.com>
2024-09-16 23:19:48 +0530

    Refactor package dependencies and add axios for API requests

--- part2/countries/src/App.jsx
+++ part2/countries/src/App.jsx
@@ -1,16 +1,67 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 
 
 
-function App() {
-  const [count, setCount] = useState(0)
-  console.log('hello')
+const ShowCountries = ({ countries }) => {
+  if (!Array.isArray(countries)) {
+    return null; // Ensure countries is an array
+  }
 
   return (
-    <>
-      hello
-    </>
+    <div>
+      {countries.map((country, index) => (
+        <div key={index}>
+          {country.name.common}
+        </div>
+      ))}
+    </div>
+  );
+};
+
+const App=()=> {
+  const [value, setValue] = useState('')
+  const [countries, setCountries] = useState([])
+  const [country, setCountry] = useState({})
+  console.log('hello')
+
+
+
+  useEffect(() => {
+    console.log('effect run, country is now', country)
+
+    console.log('fetching countries...')
+      axios
+        .get(`https://studies.cs.helsinki.fi/restcountries/api/all`)
+        .then(response => {
+          setCountries(response.data)
+          //setCountries(response.data.name.common)
+        })
+  }, [])
+
+  if(country=== null){
+    return null
+  }
+
+  const onSearch = (event) => {
+    event.preventDefault()
+    setCountry(value)
+  }
+
+  const handleChange = (event) => {
+    setValue(event.target.value)
+  }
+
+  return (
+    <div>
+      <form onSubmit={onSearch}>
+        find countries: <input value={value} onChange={handleChange} />
+        <button type="submit"></button>
+      </form>
+      <ShowCountries countries={countries}/>
+      
+    </div>
   )
 }
 
