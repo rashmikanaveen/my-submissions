a5682b128b03f68fc62a60288ee514f75acdf5d4
rashmikanaveen <rashmikanaveen734@gmail.com>
2024-09-20 02:42:28 +0530

    Refactor ShowFilterCountries component to display country details, add weather information, and fetch weather data from API

--- part2/countries/src/components/ShowFilterCountries.jsx
+++ part2/countries/src/components/ShowFilterCountries.jsx
@@ -1,8 +1,34 @@
-import React, { useState } from 'react';
+import { useState,useEffect } from 'react'
+import axios from 'axios'
+const api_key = import.meta.env.VITE_SOME_KEY
+const baseUrl = 'https://api.openweathermap.org/data/2.5/weather?q='
 
 const CountryDetails=({country})=>{
-  //console.log(country.name.common)
+  const [wetherOfCapital,setwetherOfCountry]=useState(null)
+  
+
+  
+ 
+  useEffect(() => {
+    if (country && country.capital) {
+      axios.get(`${baseUrl}${country.capital}&appid=${api_key}`)
+      .then(response => setwetherOfCountry(response.data))
+        .catch(error => {
+          console.error('Error fetching weather data:', error);
+        });
+    }
+  }, []);
+  if(wetherOfCapital===null){
+    return
+  }
+
+
+
+  console.log(wetherOfCapital)
   const imgSource=country.flags['png']
+  const weatherIcon = wetherOfCapital && wetherOfCapital.weather && wetherOfCapital.weather[0] && wetherOfCapital.weather[0].icon;
+  const wether_img_sourse = weatherIcon ? `https://openweathermap.org/img/wn/${weatherIcon}@2x.png` : '';
+  
     return (
       <div>
         <h2>{country.name.common}</h2>
@@ -18,10 +44,19 @@ const CountryDetails=({country})=>{
            
           </ul>
           <img src={imgSource} alt="" />
+
          
        
        
         </div>
+
+
+        <div>
+          <h2>Wether in {country.capital}</h2>
+          <div>temperature {wetherOfCapital.main.temp} Celcius</div>
+          <img src={wether_img_sourse} alt="" />
+        </div>
+
       </div>
     );
 
@@ -29,15 +64,16 @@ const CountryDetails=({country})=>{
 
 
 const ShowFilterCountries = ({ filterdCountries }) => {
-  const [countriesToShow, setcountriesToShow] = useState([]);
+  const [countriesToShow, setcountriesToShow] = useState(null);
   //const [countryToShow, setCountryToShow] = useState('');
+  console.log(api_key)
   
 
   
   const ShowCountry=()=>{
     
-    if(countriesToShow.length !==0){
-      console.log(countriesToShow)
+    if(countriesToShow !==null){
+      //console.log(countriesToShow)
       
       return(
         <div>
